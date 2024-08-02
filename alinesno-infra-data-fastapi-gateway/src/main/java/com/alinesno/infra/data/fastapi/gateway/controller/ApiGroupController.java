e184f509a3ba2e43c918b2f40bb048be4b7eaba2
王磊(Levi) <leviAip@gmail.com>
2024-08-02 03:53:00 -0400

    修复并优化API分组控制器，包括数据表格查询、列表获取、新增及子类排除功能。

---
+++ alinesno-infra-data-fastapi-gateway/src/main/java/com/alinesno/infra/data/fastapi/gateway/controller/ApiGroupController.java
@@ -0,0 +1,90 @@
+package com.alinesno.infra.data.fastapi.gateway.controller;
+
+import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.core.utils.StringUtils;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.data.fastapi.entity.ApiGroupEntity;
+import com.alinesno.infra.data.fastapi.service.IApiGroupService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 处理与BusinessLogEntity相关的请求的Controller。
+ * 继承自BaseController类并实现IBusinessLogService接口。
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@RestController
+@Scope(SpringInstanceScope.PROTOTYPE)
+@RequestMapping("/api/infra/data/fastapi/apiGroup")
+public class ApiGroupController extends BaseController<ApiGroupEntity, IApiGroupService> {
+
+    @Autowired
+    private IApiGroupService service;
+
+    /**
+     * 获取BusinessLogEntity的DataTables数据。
+     *
+     * @param request HttpServletRequest对象。
+     * @param model Model对象。
+     * @param page DatatablesPageBean对象。
+     * @return 包含DataTables数据的TableDataInfo对象。
+     */
+    @ResponseBody
+    @PostMapping("/datatables")
+    public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
+        log.debug("page = {}", ToStringBuilder.reflectionToString(page));
+        return this.toPage(model, this.getFeign(), page);
+    }
+
+    @GetMapping("/list")
+    public AjaxResult list(ApiGroupEntity promptCatalog) {
+        List<ApiGroupEntity> promptCatalogEntities = service.selectCatalogList(promptCatalog);
+
+        return AjaxResult.success("操作成功." , promptCatalogEntities) ;
+    }
+
+    /**
+     * 保存角色类型
+     * @return
+     */
+    @PostMapping("/insertApiGroup")
+    public AjaxResult insertApiGroup(@RequestBody ApiGroupEntity entity){
+
+        service.insertCatalog(entity) ;
+
+        return AjaxResult.success("操作成功.") ;
+    }
+
+    /**
+     * 获取到子类
+     * @param deptId
+     * @return
+     */
+    @GetMapping("/excludeChild/{id}")
+    public AjaxResult excludeChild(@PathVariable(value = "id", required = false) Long deptId)
+    {
+        List<ApiGroupEntity> depts = service.selectCatalogList(new ApiGroupEntity());
+        depts.removeIf(d -> d.getId().longValue() == deptId || ArrayUtils.contains(StringUtils.split(d.getAncestors(), ","), deptId + ""));
+        return AjaxResult.success("操作成功." , depts);
+    }
+
+
+    @Override
+    public IApiGroupService getFeign() {
+        return this.service;
+    }
+}
