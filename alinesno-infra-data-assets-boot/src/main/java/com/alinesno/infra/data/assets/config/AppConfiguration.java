16cbbb416e734eeb1397c945ef92b585c32f2a7f
王磊(Levi) <leviAip@gmail.com>
2024-08-01 20:50:00 -0400

    优化AppConfiguration类，整合日志切面与简单服务配置。

--- alinesno-infra-data-assets-boot/src/main/java/com/alinesno/infra/data/assets/config/AppConfiguration.java
+++ alinesno-infra-data-assets-boot/src/main/java/com/alinesno/infra/data/assets/config/AppConfiguration.java
@@ -3,7 +3,10 @@ package com.alinesno.infra.data.assets.config;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.alinesno.infra.data.assets.sample.ISimpleService;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +17,20 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan("com.alinesno.infra.data.assets.mapper")
 @EnableInfraSsoApi
 @Configuration
-public class AppConfiguration {
+public class AppConfiguration implements CommandLineRunner {
+
+    @Autowired
+    private ISimpleService simpleService ;
 
     @Bean
     public LogAspect getLogAspect(){
         return new LogAspect() ;
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        simpleService.catalog();
+
+    }
 }
