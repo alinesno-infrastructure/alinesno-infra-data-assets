cf22f170f449eb0e43736f77154eb76562cacb1c
王磊(Levi) <leviAip@gmail.com>
2024-08-02 09:13:00 -0400

    fix: 更新 LabelService 接口注释，明确接口功能描述。

---
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/service/ILabelService.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.data.assets.service;
+
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.assets.entity.AssetDataEntity;
+import com.alinesno.infra.data.assets.entity.LabelEntity;
+
+/**
+ * 资产标签管理
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+public interface ILabelService extends IBaseService<LabelEntity> {
+}
