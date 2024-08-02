f4171f2ab229033cd8707ed1396ecc4e891e1357
王磊(Levi) <leviAip@gmail.com>
2024-08-01 23:47:00 -0400

    修复并优化IP规则实体类的字段注释与结构。

---
+++ alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/entity/IpRuleEntity.java
@@ -0,0 +1,29 @@
+package com.alinesno.infra.data.fastapi.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@TableName("ip_rule")
+@Data
+public class IpRuleEntity extends InfraBaseEntity {
+
+    /**
+     * 状态
+     */
+    @TableField("type")
+    @Column(comment = "类型", type = MySqlTypeConstant.VARCHAR, length = 8)
+    private String type;
+
+    /**
+     * 模式
+     */
+    @TableField("ip")
+    @Column(comment = "模式", type = MySqlTypeConstant.VARCHAR, length = 255)
+    private String ip;
+}
