f5acabaf732750133efef30bbe82d2c18b463305
王磊(Levi) <leviAip@gmail.com>
2024-08-01 23:28:00 -0400

    fix: 调整API配置服务接口注释，确保与项目规范一致。

---
+++ alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IApiConfigService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.fastapi.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.fastapi.entity.ApiConfigEntity;
+
+public interface IApiConfigService extends IBaseService<ApiConfigEntity> {
+}
