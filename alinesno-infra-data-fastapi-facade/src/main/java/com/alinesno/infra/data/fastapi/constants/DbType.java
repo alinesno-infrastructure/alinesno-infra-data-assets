af9953783db7bf4ed253a80f26aa315b91a58903
王磊(Levi) <leviAip@gmail.com>
2024-08-02 04:09:00 -0400

    新增数据库类型常量MySQL及其驱动名常量。

---
+++ alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/constants/DbType.java
@@ -0,0 +1,6 @@
+package com.alinesno.infra.data.fastapi.constants;
+
+public class DbType {
+    public static final String MY_SQL ="MySQL";
+    public static final String MYSQL_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+}
