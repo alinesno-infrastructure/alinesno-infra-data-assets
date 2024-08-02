20984b0357d01de3579e409fbef6905de484eea3
王磊(Levi) <leviAip@gmail.com>
2024-08-01 21:25:00 -0400

    修复并优化 label 相关 API 文档与实现逻辑

---
+++ alinesno-infra-data-assets-ui/src/api/data/asset/label.js
@@ -0,0 +1,87 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+/**
+ * 数据库接口操作
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/data/assets/label/' ;
+var managerUrl = {
+    datatables : prefix +"datatables" ,
+    createUrl: prefix + 'add' ,
+    saveUrl: prefix + 'insertLabel' ,
+    updateUrl: prefix +"modify" ,
+    statusUrl: prefix +"changeStatus" ,
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix +"detail",
+    removeUrl: prefix + "delete" ,
+    exportUrl: prefix + "exportExcel",
+    changeField: prefix + "changeField",
+    list: prefix + "list",
+    excludeChild: prefix + "excludeChild",
+    downloadfile: prefix + "downloadfile"
+}
+
+// 查询部门列表（排除节点）
+export function listLabelExcludeChild(deptId) {
+    return request({
+        url: managerUrl.excludeChild+ '/' + deptId ,
+        method: 'get'
+    })
+}
+
+// 查询数据库列表
+export function listLabel(query) {
+    return request({
+        url: managerUrl.list,
+        method: 'get',
+        params: query
+    })
+}
+
+// 查询数据库列表
+export function datatableLabel(query) {
+    return request({
+        url: managerUrl.datatables ,
+        method: 'post',
+        params: query
+    })
+}
+
+// 查询数据库详细
+export function getLabel(databaseId) {
+    return request({
+        url: managerUrl.detailUrl + '/' + parseStrEmpty(databaseId),
+        method: 'get'
+    })
+}
+
+// 新增数据库
+export function addLabel(data) {
+    return request({
+        url: managerUrl.saveUrl ,
+        method: 'post',
+        data: data
+    })
+}
+
+// 修改数据库
+export function updateLabel(data) {
+    return request({
+        url: managerUrl.updateUrl ,
+        method: 'put',
+        data: data
+    })
+}
+
+// 删除数据库
+export function delLabel(databaseId) {
+    return request({
+        url: managerUrl.removeUrl + '/' + parseStrEmpty(databaseId),
+        method: 'delete'
+    })
+}
