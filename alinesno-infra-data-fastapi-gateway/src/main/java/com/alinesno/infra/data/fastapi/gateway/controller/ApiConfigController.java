cce9954321c6a382809fee136d27597ca70ae5cb
王磊(Levi) <leviAip@gmail.com>
2024-08-02 07:22:00 -0400

    修复并优化API配置控制器，包括数据表格处理和执行SQL更新逻辑。

---
+++ alinesno-infra-data-fastapi-gateway/src/main/java/com/alinesno/infra/data/fastapi/gateway/controller/ApiConfigController.java
@@ -0,0 +1,85 @@
+package com.alinesno.infra.data.fastapi.gateway.controller;
+
+import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.data.fastapi.entity.ApiConfigEntity;
+import com.alinesno.infra.data.fastapi.service.IApiConfigService;
+import com.alinesno.infra.data.fastapi.service.IApiGroupService;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 处理与BusinessLogEntity相关的请求的Controller。
+ * 继承自BaseController类并实现IBusinessLogService接口。
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@RestController
+@Scope(SpringInstanceScope.PROTOTYPE)
+@RequestMapping("/api/infra/data/fastapi/apiConfig")
+public class ApiConfigController extends BaseController<ApiConfigEntity, IApiConfigService> {
+
+    @Autowired
+    private IApiConfigService service;
+
+    @Autowired
+    private IApiGroupService groupService;
+
+    /**
+     * 获取BusinessLogEntity的DataTables数据。
+     *
+     * @param request HttpServletRequest对象。
+     * @param model Model对象。
+     * @param page DatatablesPageBean对象。
+     * @return 包含DataTables数据的TableDataInfo对象。
+     */
+    @ResponseBody
+    @PostMapping("/datatables")
+    public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
+        log.debug("page = {}", ToStringBuilder.reflectionToString(page));
+        return this.toPage(model, this.getFeign(), page);
+    }
+
+    @GetMapping("/catalogTreeSelect")
+    public AjaxResult catalogTreeSelect(){
+        return AjaxResult.success("success" , groupService.selectCatalogTreeList()) ;
+    }
+
+    /**
+     * 更新执行sql操作
+     * @param entity
+     * @param id
+     * @return
+     */
+    @PostMapping("/updateExecuteSql")
+    public AjaxResult updateExecuteSql(@RequestBody ApiConfigEntity entity , String id){
+
+        log.debug("id = {}" , id);
+        log.debug("entity = {}" , entity);
+
+        UpdateWrapper<ApiConfigEntity> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("id", id);
+        updateWrapper.set("datasource_id",  entity.getDatasourceId());
+        updateWrapper.set("run_sql", entity.getRunSql());
+        updateWrapper.set("open_tran", entity.isOpenTran());
+        service.update(null, updateWrapper);
+
+        return AjaxResult.success() ;
+    }
+
+    @Override
+    public IApiConfigService getFeign() {
+        return this.service;
+    }
+}
