0fbea53a2edbd9b40d89e538bc79def3d2021db2
王磊(Levi) <leviAip@gmail.com>
2024-08-02 04:09:00 -0400

    修复: 完善了 ApiHandleServiceImpl 的 handle 方法实现。

---
+++ alinesno-infra-data-fastapi-domain/src/main/java/com/alinesno/infra/data/fastapi/service/impl/ApiHandleServiceImpl.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.data.fastapi.service.impl;
+
+import com.alinesno.infra.data.fastapi.service.IApiHandleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 处理业务数据
+ */
+@Slf4j
+@Service
+public class ApiHandleServiceImpl implements IApiHandleService {
+
+    @Override
+    public Object handle() {
+        return null;
+    }
+}
