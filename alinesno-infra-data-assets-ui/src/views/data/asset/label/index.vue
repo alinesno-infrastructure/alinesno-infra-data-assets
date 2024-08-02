e823418ed2775947f517640af454dd97a512790d
王磊(Levi) <leviAip@gmail.com>
2024-08-01 21:48:00 -0400

    修复并优化标签管理界面，包括搜索、添加、修改和删除功能。

---
+++ alinesno-infra-data-assets-ui/src/views/data/asset/label/index.vue
@@ -0,0 +1,285 @@
+<template>
+   <div class="app-container">
+      <el-row :gutter="20">
+         <!--应用数据-->
+         <el-col :span="24" :xs="24">
+            <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="100px">
+               <el-form-item label="应用名称" prop="dbName">
+                  <el-input v-model="queryParams.dbName" placeholder="请输入应用名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
+               </el-form-item>
+               <el-form-item label="应用名称" prop="dbName">
+                  <el-input v-model="queryParams['condition[dbName|like]']" placeholder="请输入应用名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
+               </el-form-item>
+               <el-form-item>
+                  <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+                  <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+               </el-form-item>
+            </el-form>
+
+            <el-row :gutter="10" class="mb8">
+
+               <el-col :span="1.5">
+                  <el-button type="primary" plain icon="Plus" @click="handleAdd">新增</el-button>
+               </el-col>
+               <el-col :span="1.5">
+                  <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate">修改</el-button>
+               </el-col>
+               <el-col :span="1.5">
+                  <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete">删除</el-button>
+               </el-col>
+
+               <right-toolbar v-model:showSearch="showSearch" @queryTable="getList" :columns="columns"></right-toolbar>
+            </el-row>
+
+            <el-table v-loading="loading" :data="LabelList" @selection-change="handleSelectionChange">
+               <el-table-column type="selection" width="50" align="center" />
+
+               <!-- 业务字段-->
+               <el-table-column label="标签名称" align="center" key="labelName" prop="labelName" v-if="columns[0].visible" />
+               <el-table-column label="标识" align="center" key="labelKey" prop="labelKey" v-if="columns[1].visible" :show-overflow-tooltip="true" />
+               <el-table-column label="值" align="center" key="labelValue" prop="labelValue" v-if="columns[1].visible" :show-overflow-tooltip="true" />
+               <el-table-column label="标签类型" align="center" key="labelType" prop="labelType" v-if="columns[2].visible" :show-overflow-tooltip="true" />
+
+               <el-table-column label="状态" align="center" key="hasStatus" v-if="columns[5].visible" />
+
+               <el-table-column label="添加时间" align="center" prop="addTime" v-if="columns[6].visible" width="160">
+                  <template #default="scope">
+                     <span>{{ parseTime(scope.row.addTime) }}</span>
+                  </template>
+               </el-table-column>
+
+               <!-- 操作字段  -->
+               <el-table-column label="操作" align="center" width="150" class-name="small-padding fixed-width">
+                  <template #default="scope">
+                     <el-tooltip content="修改" placement="top" v-if="scope.row.LabelId !== 1">
+                        <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
+                           v-hasPermi="['system:Label:edit']"></el-button>
+                     </el-tooltip>
+                     <el-tooltip content="删除" placement="top" v-if="scope.row.LabelId !== 1">
+                        <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
+                           v-hasPermi="['system:Label:remove']"></el-button>
+                     </el-tooltip>
+                  </template>
+
+               </el-table-column>
+            </el-table>
+            <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList" />
+         </el-col>
+      </el-row>
+
+      <!-- 添加或修改应用配置对话框 -->
+      <el-dialog :title="title" v-model="open" width="900px" append-to-body>
+         <el-form :model="form" :rules="rules" ref="databaseRef" label-width="100px">
+            <el-row>
+               <el-col :span="24">
+                  <el-form-item label="名称" prop="dbName">
+                     <el-input v-model="form.dbName" placeholder="请输入应用名称" maxlength="50" />
+                  </el-form-item>
+               </el-col>
+            </el-row>
+            <el-row>
+               <el-col :span="24">
+                  <el-form-item label="连接" prop="jdbcUrl">
+                     <el-input v-model="form.jdbcUrl" placeholder="请输入jdbcUrl连接地址" maxlength="128" />
+                  </el-form-item>
+               </el-col>
+               <el-col :span="24">
+                  <el-form-item label="类型" prop="dbType">
+                     <el-input v-model="form.dbType" placeholder="请输入类型" maxlength="50" />
+                  </el-form-item>
+               </el-col>
+            </el-row>
+            <el-row>
+               <el-col :span="24">
+                  <el-form-item label="用户名" prop="dbUsername">
+                     <el-input v-model="form.dbUsername" placeholder="请输入连接用户名" maxlength="30" />
+                  </el-form-item>
+               </el-col>
+               <el-col :span="24">
+                  <el-form-item label="密码" prop="dbPasswd">
+                     <el-input v-model="form.dbPasswd" placeholder="请输入应用密码" type="password" maxlength="30" show-password />
+                  </el-form-item>
+               </el-col>
+            </el-row>
+
+            <el-row>
+               <el-col :span="24">
+                  <el-form-item label="备注" prop="dbDesc">
+                     <el-input v-model="form.dbDesc" placeholder="请输入应用备注"></el-input>
+                  </el-form-item>
+               </el-col>
+            </el-row>
+         </el-form>
+         <template #footer>
+            <div class="dialog-footer">
+               <el-button type="primary" @click="submitForm">确 定</el-button>
+               <el-button @click="cancel">取 消</el-button>
+            </div>
+         </template>
+      </el-dialog>
+
+   </div>
+</template>
+
+<script setup name="Label">
+
+import {
+   datatableLabel,
+   delLabel,
+   getLabel,
+   updateLabel,
+   addLabel
+} from "@/api/data/asset/label";
+
+const router = useRouter();
+const { proxy } = getCurrentInstance();
+
+// 定义变量
+const LabelList = ref([]);
+const open = ref(false);
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+const dateRange = ref([]);
+const postOptions = ref([]);
+const roleOptions = ref([]);
+
+// 列显隐信息
+const columns = ref([
+   { key: 0, label: `应用名称`, visible: true },
+   { key: 1, label: `应用描述`, visible: true },
+   { key: 2, label: `表数据量`, visible: true },
+   { key: 3, label: `类型`, visible: true },
+   { key: 4, label: `应用地址`, visible: true },
+   { key: 5, label: `状态`, visible: true },
+   { key: 6, label: `更新时间`, visible: true }
+]);
+
+const data = reactive({
+   form: {},
+   queryParams: {
+      pageNum: 1,
+      pageSize: 10,
+      dbName: undefined,
+      dbDesc: undefined
+   },
+   rules: {
+      dbName: [{ required: true, message: "名称不能为空", trigger: "blur" }] , 
+      jdbcUrl: [{ required: true, message: "连接不能为空", trigger: "blur" }],
+      dbType: [{ required: true, message: "类型不能为空", trigger: "blur" }] , 
+      dbUsername: [{ required: true , message: "用户名不能为空", trigger: "blur"}],
+      dbPasswd: [{ required: true, message: "密码不能为空", trigger: "blur" }] , 
+      dbDesc: [{ required: true, message: "备注不能为空", trigger: "blur" }] 
+   }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询应用列表 */
+function getList() {
+   loading.value = true;
+   datatableLabel(proxy.addDateRange(queryParams.value, dateRange.value)).then(res => {
+      loading.value = false;
+      LabelList.value = res.rows;
+      total.value = res.total;
+   });
+};
+
+/** 搜索按钮操作 */
+function handleQuery() {
+   queryParams.value.pageNum = 1;
+   getList();
+};
+
+/** 重置按钮操作 */
+function resetQuery() {
+   dateRange.value = [];
+   proxy.resetForm("queryRef");
+   queryParams.value.deptId = undefined;
+   proxy.$refs.deptTreeRef.setCurrentKey(null);
+   handleQuery();
+};
+/** 删除按钮操作 */
+function handleDelete(row) {
+   const LabelIds = row.id || ids.value;
+   proxy.$modal.confirm('是否确认删除应用编号为"' + LabelIds + '"的数据项？').then(function () {
+      return delLabel(LabelIds);
+   }).then(() => {
+      getList();
+      proxy.$modal.msgSuccess("删除成功");
+   }).catch(() => { });
+};
+
+/** 选择条数  */
+function handleSelectionChange(selection) {
+   ids.value = selection.map(item => item.id);
+   single.value = selection.length != 1;
+   multiple.value = !selection.length;
+};
+
+/** 重置操作表单 */
+function reset() {
+   form.value = {
+      id: undefined,
+      deptId: undefined,
+      LabelName: undefined,
+      nickName: undefined,
+      password: undefined,
+      phonenumber: undefined,
+      status: "0",
+      remark: undefined,
+   };
+   proxy.resetForm("databaseRef");
+};
+/** 取消按钮 */
+function cancel() {
+   open.value = false;
+   reset();
+};
+
+/** 新增按钮操作 */
+function handleAdd() {
+   reset();
+   open.value = true;
+   title.value = "添加应用";
+};
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+   reset();
+   const LabelId = row.id || ids.value;
+   getLabel(LabelId).then(response => {
+      form.value = response.data;
+      open.value = true;
+      title.value = "修改应用";
+   });
+};
+
+/** 提交按钮 */
+function submitForm() {
+   proxy.$refs["databaseRef"].validate(valid => {
+      if (valid) {
+         if (form.value.LabelId != undefined) {
+            updateLabel(form.value).then(response => {
+               proxy.$modal.msgSuccess("修改成功");
+               open.value = false;
+               getList();
+            });
+         } else {
+            addLabel(form.value).then(response => {
+               proxy.$modal.msgSuccess("新增成功");
+               open.value = false;
+               getList();
+            });
+         }
+      }
+   });
+};
+
+getList();
+
+</script>
