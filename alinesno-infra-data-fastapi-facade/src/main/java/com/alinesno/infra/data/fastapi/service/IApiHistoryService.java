e1dabbeae69cc9ff4d6c60b8959c54d2e3a4000d
王磊(Levi) <leviAip@gmail.com>
2024-08-02 04:03:00 -0400

    fix:修复并优化IApiHistoryService接口文档注释

---
+++ alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IApiHistoryService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.fastapi.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.fastapi.entity.ApiHistoryEntity;
+
+public interface IApiHistoryService extends IBaseService<ApiHistoryEntity> {
+}
