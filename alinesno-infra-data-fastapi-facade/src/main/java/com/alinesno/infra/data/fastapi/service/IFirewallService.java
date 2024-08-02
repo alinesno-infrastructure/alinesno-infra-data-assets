0eb8b9aed37f737eef72c81f70de7821968314b5
王磊(Levi) <leviAip@gmail.com>
2024-08-02 01:24:00 -0400

    fix:完善防火墙服务接口定义

---
+++ alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IFirewallService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.fastapi.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.fastapi.entity.FirewallEntity;
+
+public interface IFirewallService extends IBaseService<FirewallEntity> {
+}
