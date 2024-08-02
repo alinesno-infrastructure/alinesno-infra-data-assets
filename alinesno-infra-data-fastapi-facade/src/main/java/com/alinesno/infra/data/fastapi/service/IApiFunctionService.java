4312ee88484b46a11630b08ce5cb876bc14dbe8b
王磊(Levi) <leviAip@gmail.com>
2024-08-02 02:19:00 -0400

    修复: 更新API功能服务接口注释与文档。

---
+++ alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IApiFunctionService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.fastapi.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.fastapi.entity.ApiFunctionEntity;
+
+public interface IApiFunctionService extends IBaseService<ApiFunctionEntity> {
+}
