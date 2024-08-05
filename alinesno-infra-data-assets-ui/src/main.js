080d10bc1705ad7c590dc560444e7ae28459a11c
王磊(Levi) <leviAip@gmail.com>
2024-08-05 19:32:00 -0400

    优化src/main.js文件，引入并注册所需Vue组件及工具函数。

--- alinesno-infra-data-assets-ui/src/main.js
+++ alinesno-infra-data-assets-ui/src/main.js
@@ -24,7 +24,7 @@ import elementIcons from '@/components/SvgIcon/svgicon'
 import './permission' // permission control
 
 import { useDict } from '@/utils/dict'
-import { parseTime, resetForm, addDateRange, handleTree, selectDictLabel, selectDictLabels } from '@/utils/ruoyi'
+import { parseTime, formatBytes , resetForm, addDateRange, handleTree, selectDictLabel, selectDictLabels } from '@/utils/ruoyi'
 
 // 分页组件
 import Pagination from '@/components/Pagination'
@@ -61,6 +61,7 @@ app.config.globalProperties.GLOBAL_FUN = GLOBAL_FUN
 app.config.globalProperties.useDict = useDict
 app.config.globalProperties.download = download
 app.config.globalProperties.parseTime = parseTime
+app.config.globalProperties.formatBytes = formatBytes
 app.config.globalProperties.resetForm = resetForm
 app.config.globalProperties.handleTree = handleTree
 app.config.globalProperties.addDateRange = addDateRange
