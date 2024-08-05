d80aac429bdb72a8ce1d11e471bae25d061570c4
王磊(Levi) <leviAip@gmail.com>
2024-08-05 18:24:00 -0400

    优化AppConfiguration类，整合日志切面与服务配置，并增加资产目录、标签分类与示例数据初始化。

--- alinesno-infra-data-assets-boot/src/main/java/com/alinesno/infra/data/assets/config/AppConfiguration.java
+++ alinesno-infra-data-assets-boot/src/main/java/com/alinesno/infra/data/assets/config/AppConfiguration.java
@@ -8,14 +8,16 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 服务配置
  */
 @EnableActable
-@MapperScan("com.alinesno.infra.data.assets.mapper")
+@MapperScan(basePackages = {"com.alinesno.infra.data.assets.mapper" , "com.alinesno.infra.data.fastapi.mapper"})
 @EnableInfraSsoApi
+@ComponentScan(basePackages = {"com.alinesno.infra.data.assets" , "com.alinesno.infra.data.fastapi"})
 @Configuration
 public class AppConfiguration implements CommandLineRunner {
 
@@ -35,6 +37,9 @@ public class AppConfiguration implements CommandLineRunner {
 
         // 初始化标签分类
         simpleService.label();
+
+        // 示例资产数据
+        simpleService.data();
 
     }
 }
