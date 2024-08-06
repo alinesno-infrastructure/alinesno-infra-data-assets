f67a0c86a8b9e39ba2f0a4e5808140912f9b2b68
梁明树(Nina) <ninaAip@gmail.com>
2024-08-06 19:02:00 -0400

    优化SQL编辑器组件，调整描述并优化布局。

--- alinesno-infra-data-assets-ui/src/views/data/fastapi/api/sqlEditor.vue
+++ alinesno-infra-data-assets-ui/src/views/data/fastapi/api/sqlEditor.vue
@@ -1,71 +1,71 @@
 <template>
-    <div>
-        <el-form :model="form" :rules="rules" ref="executeRef" label-width="120px">
-            <el-row>
-               <el-col :span="24">
-                  <el-form-item style="width: 100%;" label="数据库源" prop="datasourceId">
-                     <el-tree-select
-                        v-model="form.datasourceId"
-                        :data="datasourceList"
-                        :props="{ value: 'id', label: 'dbDesc'}"
-                        value-key="id"
-                        placeholder="请选择归属类型"
-                        check-strictly
-                     />
-                  </el-form-item>
-               </el-col>
-            </el-row>
+  <div>
+    <el-form :model="form" :rules="rules" ref="executeRef" label-width="120px">
+<!--      <el-row>
+        <el-col :span="24">
+          <el-form-item style="width: 100%;" label="数据库源" prop="datasourceId">
+            <el-tree-select
+                v-model="form.datasourceId"
+                :data="datasourceList"
+                :props="{ value: 'id', label: 'dbDesc'}"
+                value-key="id"
+                placeholder="请选择归属数据库源"
+                check-strictly
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>-->
 
-            <el-row>
-               <el-col :span="24">
-                   <el-form-item label="执行SQL" prop="runSql">
-                     <el-tabs
-                         v-model="editableTabsValue"
-                         type="card"
-                         editable
-                         @edit="handleTabsEdit"
-                         tab-position="top"
-                     >
-                   <el-tab-pane
-                       v-for="(item, index) in editableTabs"
-                       :key="item.index"
-                       :label="item.name"
-                       :name="item.name"
-                   >
-                     <el-input
-                         type="textarea"
-                         :autosize="{minRows:10,maxRows:20}"
-                         v-model="item.runSql"
-                         placeholder="请输入指令名称"
-                         style="width: 865px"
-                         maxlength="500" />
-                   </el-tab-pane>
-                 </el-tabs>
-                 </el-form-item>
-               </el-col>
-            </el-row>
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="执行SQL" prop="runSql">
+            <el-tabs
+                v-model="editableTabsValue"
+                type="card"
+                editable
+                @edit="handleTabsEdit"
+                tab-position="top"
+            >
+              <el-tab-pane
+                  v-for="(item, index) in editableTabs"
+                  :key="item.index"
+                  :label="item.name"
+                  :name="item.name"
+              >
+                <el-input
+                    type="textarea"
+                    :autosize="{minRows:10,maxRows:20}"
+                    v-model="item.runSql"
+                    placeholder="请输入指令名称"
+                    style="width: 865px"
+                    maxlength="500" />
+              </el-tab-pane>
+            </el-tabs>
+          </el-form-item>
+        </el-col>
+      </el-row>
 
-            <el-form-item label="事务" prop="openTran">
-                  <el-switch
-                  v-model="form.openTran"
-                  :active-value="true"
-                  :inactive-value="false"
-                  />
-            </el-form-item>
+      <el-form-item label="事务" prop="openTran">
+        <el-switch
+            v-model="form.openTran"
+            :active-value="true"
+            :inactive-value="false"
+        />
+      </el-form-item>
 
-         </el-form>
-    </div>
+    </el-form>
+  </div>
 
 </template>
 
 <script setup name="SqlEditor">
 
 import {
-   allDatasource,
+  allDatasource,
 } from "@/api/data/fastapi/datasource";
 
 import {
-   updateExecuteSql
+  updateExecuteSql
 } from "@/api/data/fastapi/apiConfig";
 
 const router = useRouter();
@@ -80,23 +80,23 @@ const props = defineProps({
 })
 
 const data = reactive({
-   form: {
-     datasourceId:'',
-     runSql:'',
-     openTran:0,
-   },
-   queryParams: {
-      pageNum: 1,
-      pageSize: 10,
-      datasourceId: '',
-      runSql: '',
-      openTran: 0
-   },
-   rules: {
-      datasourceId: [{ required: true, message: "数据源不能为空", trigger: "blur" }],
-      runSql: [{ required: true, message: "SQL不能为空", trigger: "blur" }] ,
-      openTran: [{ required: true, message: "事务不能为空", trigger: "blur" }]
-   }
+  form: {
+    datasourceId:'',
+    runSql:'',
+    openTran:0,
+  },
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    datasourceId: '',
+    runSql: '',
+    openTran: 0
+  },
+  rules: {
+    datasourceId: [{ required: true, message: "数据源不能为空", trigger: "blur" }],
+    runSql: [{ required: true, message: "SQL不能为空", trigger: "blur" }] ,
+    openTran: [{ required: true, message: "事务不能为空", trigger: "blur" }]
+  }
 });
 
 const { queryParams, form, rules } = toRefs(data);
@@ -113,7 +113,7 @@ onMounted(() => {
   form.value.runSql = props.currentApiConfigContent.runSql ;
   form.value.openTran = props.currentApiConfigContent.openTran ;
   if ( props.currentApiConfigContent.runSql != null && props.currentApiConfigContent.runSql != undefined && props.currentApiConfigContent.runSql != '' ){
-     editableTabs.value = JSON.parse(props.currentApiConfigContent.runSql)
+    editableTabs.value = JSON.parse(props.currentApiConfigContent.runSql)
   }
 })
 
@@ -130,9 +130,9 @@ function  reset() {
 
 /** 获取到所有数据源 */
 function handleAllDatasource() {
-   allDatasource().then(res => {
-      datasourceList.value = res.data ;
-   })
+  allDatasource().then(res => {
+    datasourceList.value = res.data ;
+  })
 }
 
 /** 提交按钮 */
@@ -198,7 +198,7 @@ handleAllDatasource() ;
 
 /** 提供对外访问接口 */
 defineExpose({
-   submitForm 
+  submitForm
 })
 
 </script>
