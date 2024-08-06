66a3c9ac6241a6d14d397a59d6b004b6582ad98e
王磊(Levi) <leviAip@gmail.com>
2024-08-06 07:06:00 -0400

    fix: 更新SimpleServiceImpl，添加数据服务相关逻辑及初始化资产目录和标签。

--- alinesno-infra-data-assets-initialize/src/main/java/com/alinesno/infra/data/assets/sample/impl/SimpleServiceImpl.java
+++ alinesno-infra-data-assets-initialize/src/main/java/com/alinesno/infra/data/assets/sample/impl/SimpleServiceImpl.java
@@ -1,10 +1,13 @@
 package com.alinesno.infra.data.assets.sample.impl;
 
 import com.alinesno.infra.data.assets.entity.AssetCatalogEntity;
+import com.alinesno.infra.data.assets.entity.AssetDataEntity;
 import com.alinesno.infra.data.assets.entity.LabelEntity;
 import com.alinesno.infra.data.assets.sample.ISimpleService;
-import com.alinesno.infra.data.assets.sample.utils.DataAssetsBuilder;
+import com.alinesno.infra.data.assets.sample.builder.DataAssetGenerator;
+import com.alinesno.infra.data.assets.sample.builder.DataAssetsBuilder;
 import com.alinesno.infra.data.assets.service.IAssetCatalogService;
+import com.alinesno.infra.data.assets.service.IAssetDataService;
 import com.alinesno.infra.data.assets.service.ILabelService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +27,9 @@ public class SimpleServiceImpl implements ISimpleService {
     @Autowired
     private ILabelService labelService;
 
+    @Autowired
+    private IAssetDataService dataService ;
+
     @Override
     public void catalog() {
 
@@ -37,6 +43,11 @@ public class SimpleServiceImpl implements ISimpleService {
 
     @Override
     public void data() {
+
+        if(dataService.count(new LambdaQueryWrapper<>()) == 0){
+            List<AssetDataEntity> assetDataList = DataAssetGenerator.generateSampleData();
+            dataService.saveBatch(assetDataList) ;
+        }
 
     }
 
