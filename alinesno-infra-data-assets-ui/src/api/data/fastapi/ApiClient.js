064933da67d0c5e3c472864ea86f643facbb6dd1
李娜(Allen) <allenAip@gmail.com>
2024-08-08 02:18:00 -0400

    新增API客户端操作接口实现

---
+++ alinesno-infra-data-assets-ui/src/api/data/fastapi/ApiClient.js
@@ -0,0 +1,77 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+/**
+ * 客户端操作
+ * 
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/data/fastapi/ApiClient/' ;
+var managerUrl = {
+    datatables : prefix +"datatables" ,
+    createUrl: prefix + 'add' ,
+    saveUrl: prefix + 'save' ,
+    updateUrl: prefix +"modify" ,
+    statusUrl: prefix +"changeStatus" ,
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix +"detail",
+    removeUrl: prefix + "delete" ,
+    changeField: prefix + "changeField"
+}
+
+// 修改字段
+export function changStatusField(data){
+  return request({
+    url: managerUrl.changeField ,
+    method: 'post',
+    data: data
+  })
+}
+
+
+// 查询数据库列表
+export function listApiClient(query) {
+    debugger
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询数据库详细
+export function getApiClient(databaseId) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(databaseId),
+    method: 'get'
+  })
+}
+
+// 新增数据库
+export function addApiClient(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改数据库
+export function updateApiClient(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除数据库
+export function delApiClient(databaseId) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(databaseId),
+    method: 'delete'
+  })
+}
