b495f9aec4be907bb2ac17081bc38336c398ceb2
李娜(Allen) <allenAip@gmail.com>
2024-08-08 05:54:00 -0400

    新增API客户端管理界面，包括搜索、添加、修改和删除功能。

---
+++ alinesno-infra-data-assets-ui/src/views/data/fastapi/apiclient/index.vue
@@ -0,0 +1,307 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch">
+      <el-form-item label="客户端名称" prop="clientName">
+        <el-input
+            v-model="queryParams.clientName"
+            placeholder="请输入客户端名称"
+            clearable
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="客户端ID" prop="clientId">
+        <el-input
+            v-model="queryParams.clientId"
+            placeholder="请输入客户端ID"
+            clearable
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button type="primary" plain icon="Plus" @click="handleAdd">新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate">修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete">删除</el-button>
+      </el-col>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="ApiClientList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="50" :align="'center'" />
+      <!-- 业务字段-->
+      <el-table-column label="客户端名称" align="left" key="clientName" prop="clientName" v-if="columns[0].visible" />
+      <el-table-column label="客户端ID" align="left" key="clientId" prop="clientId" v-if="columns[1].visible" />
+      <el-table-column label="密钥" align="left" key="secret" prop="secret" v-if="columns[2].visible" />
+<!--      <el-table-column label="备注" align="left" key="remark" prop="remark" width="170" v-if="columns[3].visible" />-->
+      <el-table-column label="Token过期时间" align="left" key="expiryTime" prop="expiryTime"  width="170" v-if="columns[4].visible" />
+      <el-table-column label="调用次数" align="left" key="useCount" prop="useCount" width="80" v-if="columns[5].visible" />
+      <el-table-column label="状态" align="center" width="100" key="hasStatus" v-if="columns[7].visible">
+        <template #default="scope">
+          <el-switch v-model="scope.row.hasStatus" :active-value="0" :inactive-value="1"
+                     @change="handleChangStatusField('hasStatus', scope.row.hasStatus, scope.row.id)" />
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" align="center" prop="addTime" width="170">
+        <template #default="scope">
+          <span>{{ parseTime(scope.row.addTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" width="200" class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-button
+              type="text"
+              icon="Edit"
+              @click="handleUpdate(scope.row)"
+          >修改</el-button>
+          <el-button
+              v-if="scope.row.parentId != 0"
+              type="text"
+              icon="Delete"
+              @click="handleDelete(scope.row)"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 添加或修改分组对话框 -->
+    <el-dialog :title="title" v-model="open" width="600px" append-to-body>
+      <el-form ref="databaseRef" :model="form" :rules="rules" label-width="80px">
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="名称" prop="clientName">
+              <el-input v-model="form.clientName" placeholder="请输入客户端名称" maxlength="50" style="width: 720px;" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="客户端ID" prop="clientId">
+              <el-input v-model="form.clientId" placeholder="请输入客户端ID" maxlength="128" style="width: 720px;"/>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="密钥" prop="secret">
+              <el-input v-model="form.secret" placeholder="请输入密钥" maxlength="128" style="width: 720px;"/>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="备注" prop="remark">
+              <el-input v-model="form.remark" placeholder="请输入备注" maxlength="128" style="width: 720px;"/>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup name="ApiClient">
+
+import {
+  listApiClient,
+  delApiClient,
+  getApiClient,
+  updateApiClient,
+  addApiClient,
+  changStatusField
+} from "@/api/data/fastapi/ApiClient";
+
+import {v4 as uuidv4} from 'uuid' ;
+
+
+const router = useRouter();
+const { proxy } = getCurrentInstance();
+
+// 定义变量
+const ApiClientList = ref([]);
+const open = ref(false);
+
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const names = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+const dateRange = ref([]);
+
+
+// 列显隐信息
+const columns = ref([
+  { key: 0, label: `客户端名称`, visible: true },
+  { key: 1, label: `客户端ID`, visible: true },
+  { key: 2, label: `密钥`, visible: true },
+  { key: 3, label: `备注`, visible: true },
+  { key: 4, label: `Token过期时间`, visible: true },
+  { key: 5, label: `调用次数`, visible: true },
+  { key: 6, label: `状态`, visible: true },
+  { key: 7, label: `添加时间`, visible: true }
+]);
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    clientName: undefined,
+    clientId: undefined,
+    secret: undefined,
+    remark: undefined
+  },
+  rules: {
+    clientName: [{ required: true, message: "名称不能为空", trigger: "blur" }],
+    clientId: [{ required: true, message: "路径不能为空", trigger: "blur" }],
+    secret: [{ required: true, message: "服务目录不能为空", trigger: "blur" }],
+    remark: [{ required: false, message: "内容类型不能为空", trigger: "blur" }],
+    expiryTime: [{ required: false, message: "私有不能为空", trigger: "blur" }]
+  }
+});
+
+
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询指令列表 */
+function getList() {
+  loading.value = true;
+  listApiClient(proxy.addDateRange(queryParams.value, dateRange.value)).then(res => {
+    debugger
+    loading.value = false;
+    ApiClientList.value = res.rows;
+    total.value = res.total;
+  });
+};
+
+
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+};
+
+/** 重置按钮操作 */
+function resetQuery() {
+  dateRange.value = [];
+  proxy.resetForm("queryRef");
+  handleQuery();
+};
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const ApiClientIds = row.id || ids.value;
+  const clientNames = row.clientName || names.value;
+  proxy.$modal.confirm('是否确认删除客户端名称为"' + clientNames + '"的数据项？').then(function () {
+    return delApiClient(ApiClientIds);
+  }).then(() => {
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => { });
+};
+
+/** 选择条数  */
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.id);
+  names.value = selection.map(item => item.clientName);
+  single.value = selection.length != 1;
+  multiple.value = !selection.length;
+};
+
+/** 重置操作表单 */
+function reset() {
+  form.value = {
+    id: undefined,
+    clientName: undefined,
+    clientId: uuidv4().replace(/-/g,""),
+    secret:  uuidv4().replace(/-/g,""),
+    remark: undefined,
+    expiryTime: undefined,
+    useCount: undefined
+  };
+  proxy.resetForm("databaseRef");
+};
+/** 取消按钮 */
+function cancel() {
+  open.value = false;
+  reset();
+};
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加客户端";
+};
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const ApiClientId = row.id || ids.value;
+  getApiClient(ApiClientId).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改客户端";
+  });
+};
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["databaseRef"].validate(valid => {
+    if (valid) {
+      if (form.value.id != undefined) {
+        updateApiClient(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        });
+      } else {
+        addApiClient(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        });
+      }
+    }
+  });
+};
+
+
+
+const handleChangStatusField = async (field, value, id) => {
+  // 判断tags值 这样就不会进页面时调用了
+  const res = await changStatusField({
+    field: field,
+    value: value ? 1 : 0,
+    id: id
+  }).catch(() => { })
+  if (res && res.code == 200) {
+    // 刷新表格
+    getList()
+  }
+}
+
+getList();
+</script>
