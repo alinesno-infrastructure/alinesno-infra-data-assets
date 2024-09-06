b1dade8380b583bae934c5fb014345b39f179d5e
周健(James) <jamesAip@gmail.com>
2024-09-06 10:05:00 -0400

    新增DataMetricMapper接口。

---
+++ alinesno-infra-data-assets-metrics/src/main/java/com/alinesno/infra/data/assets/metrics/mapper/DataMetricMapper.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.assets.metrics.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.assets.metrics.entity.DataMetricEntity;
+
+public interface DataMetricMapper extends IBaseMapper<DataMetricEntity> {
+}
