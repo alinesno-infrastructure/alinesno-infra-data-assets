d70d713efc25b4247c09665e62af623faff0e2a6
周健(James) <jamesAip@gmail.com>
2024-09-06 02:58:00 -0400

    新增 IDataMetricService 接口。

---
+++ alinesno-infra-data-assets-metrics/src/main/java/com/alinesno/infra/data/assets/metrics/service/IDataMetricService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.assets.metrics.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.assets.metrics.entity.DataMetricEntity;
+
+public interface IDataMetricService extends IBaseService<DataMetricEntity> {
+}
