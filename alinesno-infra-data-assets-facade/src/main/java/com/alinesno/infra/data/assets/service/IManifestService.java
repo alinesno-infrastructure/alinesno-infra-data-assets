da82cd790eadfecf0d9dd614eee034c516cae281
张婷(Tina) <tinaAip@gmail.com>
2024-11-22 00:17:33 +0800

    feat: 添加资产清单管理接口 IManifestService

---
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/service/IManifestService.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.data.assets.service;
+
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.assets.entity.LabelEntity;
+import com.alinesno.infra.data.assets.entity.ManifestEntity;
+
+/**
+ * 资产清单管理
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+public interface IManifestService extends IBaseService<ManifestEntity> {
+}
