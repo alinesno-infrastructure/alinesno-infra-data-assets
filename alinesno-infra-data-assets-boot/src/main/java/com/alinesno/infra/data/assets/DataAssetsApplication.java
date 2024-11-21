4ed72e996b91009db398b84ea25bc30f0b9098b6
张婷(Tina) <tinaAip@gmail.com>
2024-11-22 00:17:33 +0800

    移除不必要的依赖导入，优化启动类代码结构

--- alinesno-infra-data-assets-boot/src/main/java/com/alinesno/infra/data/assets/DataAssetsApplication.java
+++ alinesno-infra-data-assets-boot/src/main/java/com/alinesno/infra/data/assets/DataAssetsApplication.java
@@ -1,15 +1,11 @@
 package com.alinesno.infra.data.assets;
 
-import com.alinesno.infra.common.facade.enable.EnableActable;
-import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 集成一个Java开发示例工具
- * @author LuoAnDong
+ * @author luoxiaodong
  * @since 2023年8月3日 上午6:23:43
  */
 @SpringBootApplication
