1593a909546e1b9091f7955e70e3779f2cc0375e
张婷(Tina) <tinaAip@gmail.com>
2024-11-22 00:17:33 +0800

    feat:新增 ManifestMapper 接口，用于资产收藏管理。

---
+++ alinesno-infra-data-assets-domain/src/main/java/com/alinesno/infra/data/assets/mapper/ManifestMapper.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.data.assets.mapper;
+
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.assets.entity.LabelEntity;
+import com.alinesno.infra.data.assets.entity.ManifestEntity;
+
+/**
+ * 资产收藏Mapper接口
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+public interface ManifestMapper extends IBaseMapper<ManifestEntity> {
+}
