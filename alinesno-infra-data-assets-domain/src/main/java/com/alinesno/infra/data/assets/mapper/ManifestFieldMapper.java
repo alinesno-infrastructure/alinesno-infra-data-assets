f96aadb18303ecf36159d6fe1481ad416ac4b194
张婷(Tina) <tinaAip@gmail.com>
2024-11-22 00:17:33 +0800

    feat:新增 ManifestFieldMapper 接口，实现资产收藏数据操作。

---
+++ alinesno-infra-data-assets-domain/src/main/java/com/alinesno/infra/data/assets/mapper/ManifestFieldMapper.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.data.assets.mapper;
+
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.assets.entity.ManifestEntity;
+import com.alinesno.infra.data.assets.entity.ManifestFieldEntity;
+
+/**
+ * 资产收藏Mapper接口
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+public interface ManifestFieldMapper extends IBaseMapper<ManifestFieldEntity> {
+}
