9eb4e9987ec3149c32c3c7fd34300ee18d6a399e
林晓(Lily) <lilyAip@gmail.com>
2024-11-23 00:16:44 +0800

    feat: 增加IManifestService接口方法，支持数据导入和CSV文件处理。

--- alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/service/IManifestService.java
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/service/IManifestService.java
@@ -2,8 +2,13 @@ package com.alinesno.infra.data.assets.service;
 
 
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.data.assets.entity.LabelEntity;
+import com.alinesno.infra.data.assets.api.TableMetrics;
 import com.alinesno.infra.data.assets.entity.ManifestEntity;
+import com.alinesno.infra.data.assets.entity.ManifestFieldEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 资产清单管理
@@ -12,4 +17,31 @@ import com.alinesno.infra.data.assets.entity.ManifestEntity;
  * @since 1.0.0
  */
 public interface IManifestService extends IBaseService<ManifestEntity> {
+
+    /**
+     * 保存dataList数据到Postgresql数据资产库中
+     *
+     * @param dataList
+     * @param fieldList
+     * @param tableName
+     */
+    TableMetrics saveToDatahouse(List<Map<String, String>> dataList, List<ManifestFieldEntity> fieldList, String tableName);
+
+    /**
+     * 保存csv文件到Postgresql数据资产库
+     * @param file
+     * @param fieldList
+     * @param tableName
+     * @return
+     */
+    TableMetrics saveToDatahouse(MultipartFile file, List<ManifestFieldEntity> fieldList, String tableName);
+
+    /**
+     * 保存csv文件到Postgresql数据资产库
+     * @param file
+     * @param fieldList
+     * @param tableName
+     * @return
+     */
+    TableMetrics saveCvsToDatahouse(MultipartFile file, List<ManifestFieldEntity> fieldList, String tableName);
 }
