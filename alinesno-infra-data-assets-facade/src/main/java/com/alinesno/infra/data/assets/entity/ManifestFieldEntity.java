e803cd579b0c9b3b6894155f4812b2c476178a97
林晓(Lily) <lilyAip@gmail.com>
2024-11-23 00:16:44 +0800

    优化 ManifestFieldEntity 类字段定义及注释

--- alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/entity/ManifestFieldEntity.java
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/entity/ManifestFieldEntity.java
@@ -5,36 +5,32 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
 import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-
-@Builder
 @EqualsAndHashCode(callSuper = true)
 @Data
+@NoArgsConstructor
 @TableName("assets_manifest_field")
 @TableComment("数据清单")
 public class ManifestFieldEntity extends InfraBaseEntity {
 
-    @Column(length = 50 , comment = "清单ID" , isNull = false)
+    @Column(length = 32 , comment = "清单ID" , isNull = false)
     @TableField("manifest_id")
     private Long manifestId = 0L;
 
     @Column(length = 50 , comment = "字段名" , isNull = false)
     @TableField
-    private String name;
+    private String fieldName;
 
-    @Column(length = 50 , comment = "字段类型" , isNull = false)
+    @Column(length = 16 , comment = "字段类型" , isNull = false)
     @TableField
-    private String type;
+    private String fieldType;
 
-    @Column(length = 50 , comment = "字段长度" , isNull = false)
+    @Column(length = 11 , comment = "字段长度" , isNull = false)
     @TableField
-    private Integer length;
+    private Integer filedLength;
 
     @Column(length = 50 , comment = "是否为空")
     @TableField
@@ -46,5 +42,6 @@ public class ManifestFieldEntity extends InfraBaseEntity {
 
     // 字段注释，可选
     @TableField
-    private String comment;
+    @Column(length = 200 , comment = "字段注释")
+    private String fieldComment;
 }
