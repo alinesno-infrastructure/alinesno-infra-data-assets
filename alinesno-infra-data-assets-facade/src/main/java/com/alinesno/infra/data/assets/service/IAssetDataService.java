160b59c629cc9bfc596f10f727dfb20b25d1a402
林晓(Lily) <lilyAip@gmail.com>
2024-11-23 00:16:44 +0800

    feat:在 `IAssetDataService` 接口中新增查询资产数据的方法。

--- alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/service/IAssetDataService.java
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/service/IAssetDataService.java
@@ -1,9 +1,13 @@
 package com.alinesno.infra.data.assets.service;
 
 
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.data.assets.entity.AssetCollectionEntity;
+import com.alinesno.infra.data.assets.api.AssetsTableDataInfo;
 import com.alinesno.infra.data.assets.entity.AssetDataEntity;
+import com.alinesno.infra.data.assets.entity.ManifestFieldEntity;
+
+import java.util.List;
 
 /**
  * 资产收藏Service接口
@@ -12,4 +16,15 @@ import com.alinesno.infra.data.assets.entity.AssetDataEntity;
  * @since 1.0.0
  */
 public interface IAssetDataService extends IBaseService<AssetDataEntity> {
+
+    /**
+     * 查询资产数据
+     *
+     * @param page
+     * @param manifestId
+     * @param fieldList
+     * @return
+     */
+    AssetsTableDataInfo findManifestData(DatatablesPageBean page, String manifestId, List<ManifestFieldEntity> fieldList);
+
 }
