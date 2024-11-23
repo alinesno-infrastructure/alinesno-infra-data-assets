6223c0c13089b1d9233318726d08b9dfc254b2f6
梁明树(Nina) <ninaAip@gmail.com>
2024-11-24 00:14:01 +0800

    feat:新增 `AssetDataConstants` 类，定义默认字段列表。

---
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/constants/AssetDataConstants.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.assets.constants;
+
+import com.alinesno.infra.data.assets.api.TableFieldRequestDto;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AssetDataConstants {
+    public static final List<TableFieldRequestDto> DEFAULT_FIELDS = Arrays.asList(
+        new TableFieldRequestDto("id", "string", 64, false, true, "唯一ID号"),
+        new TableFieldRequestDto("add_time", "date", 18, false, false, "添加时间"),
+        new TableFieldRequestDto("operator_id", "string", 32, false, false, "操作员"),
+        new TableFieldRequestDto("org_id", "string", 32, false, false, "所属组织"),
+        new TableFieldRequestDto("department_id", "string", 32, false, false, "所属部门")
+    );
+}
