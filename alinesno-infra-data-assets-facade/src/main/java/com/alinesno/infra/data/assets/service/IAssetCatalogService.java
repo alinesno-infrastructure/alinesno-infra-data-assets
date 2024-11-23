aa054f5d826dafe3d92cb79837d0f29bb385bb10
梁明树(Nina) <ninaAip@gmail.com>
2024-11-24 00:14:01 +0800

    feat:在 `IAssetCatalogService` 接口中添加权限查询参数。

--- alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/service/IAssetCatalogService.java
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/service/IAssetCatalogService.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.data.assets.service;
 
 
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.data.assets.api.TreeSelectDto;
 import com.alinesno.infra.data.assets.entity.AssetCatalogEntity;
@@ -17,10 +18,12 @@ public interface IAssetCatalogService extends IBaseService<AssetCatalogEntity> {
 
     /**
      * 查询出指令类型列表
+     *
      * @param promptCatalog
+     * @param query
      * @return
      */
-    List<AssetCatalogEntity> selectCatalogList(AssetCatalogEntity promptCatalog);
+    List<AssetCatalogEntity> selectCatalogList(AssetCatalogEntity promptCatalog, PermissionQuery query);
 
     /**
      * 保存用户类型
@@ -32,7 +35,7 @@ public interface IAssetCatalogService extends IBaseService<AssetCatalogEntity> {
      * 查询类型列表树
      * @return
      */
-    List<TreeSelectDto> selectCatalogTreeList();
+    List<TreeSelectDto> selectCatalogTreeList(PermissionQuery query);
 
     /**
      * 查询顶级类型列表
@@ -45,5 +48,5 @@ public interface IAssetCatalogService extends IBaseService<AssetCatalogEntity> {
      * 查询清单列表信息
      * @return
      */
-    List<TreeSelectDto> catalogManifestTreeSelect();
+    List<TreeSelectDto> catalogManifestTreeSelect(PermissionQuery query);
 }
