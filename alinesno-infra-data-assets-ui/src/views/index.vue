afbc93698dfde75e86e7e5fe72df50dc6eb2618f
李伟(Michael) <michaelAip@gmail.com>
2024-11-26 00:17:08 +0800

    feat:优化数据资产管理页面布局并添加数据接入密钥复制功能。

--- alinesno-infra-data-assets-ui/src/views/index.vue
+++ alinesno-infra-data-assets-ui/src/views/index.vue
@@ -3,17 +3,20 @@
 
     <el-row>
       <el-col :span="24">
-        <div class="aip-appinfo-header">
-          <div class="header-icon-banner">
-            <i class="fa-solid fa-layer-group"></i>
-          </div>
+        <div class="aip-appinfo-header" style="display: flex;justify-content: space-between;">
           <div class="icon">
-            <div class="title">
-                  {{ currentEnvClusterObj.appName }}
+            <div class="header-icon-banner">
+              <i class="fa-solid fa-layer-group"></i>
             </div>
-            <div class="title-desc">
-                  {{ currentEnvClusterObj.desc }}
+            <div class="title" style="display: flex;flex-direction: column;gap: 8px;">
+                {{ currentEnvClusterObj.appName }}
+                <div class="title-desc">
+                      {{ currentEnvClusterObj.desc }}
+                </div>
             </div>
+          </div>
+          <div style="font-size: 14px;color: #444;">
+            数据接入密钥 <el-button type="primary" v-copyText="apiKey" text bg ><el-icon><CopyDocument /></el-icon> 复制</el-button>
           </div>
         </div>
       </el-col>
@@ -33,18 +36,29 @@
 
 <script setup name="Index">
 
-
-// import OperationWorkspaceCountStaitcs from './operation-workspace/countStatics.vue'
-
 import OperationWorkspaceApps from './operation-workspace/apps.vue'
 import OperationWorkspaceService from './operation-workspace/service.vue'
 
+import { getApiKey} from '@/api/data/asset/dashboard'
 
 const currentEnvClusterObj = ref({
   appName : '数据资产管理服务'  , 
   apiServerUrl: 'http://portal.infra.linesno.com' , 
   desc: '数据经采集、集成、加工等流程处理完成后，可以在数据资产模块进行系统化管理'
 }) 
+
+let apiKey = ref('sk-xxxx')
+
+const handleApiKey = () => {
+  getApiKey().then(response => {
+    console.log(response)
+    apiKey.value = response.data
+  })
+}
+
+onMounted(() => {
+  handleApiKey()
+})
 
 </script>
 
