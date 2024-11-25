34ef85dfcd3bc7f458521221b1737a60298b2505
李伟(Michael) <michaelAip@gmail.com>
2024-11-26 00:17:08 +0800

    feat:在LabelController中新增标签保存方法及注解@DataPermissionSave。

--- alinesno-infra-data-assets-gateway/src/main/java/com/alinesno/infra/data/assets/api/controller/LabelController.java
+++ alinesno-infra-data-assets-gateway/src/main/java/com/alinesno/infra/data/assets/api/controller/LabelController.java
@@ -1,14 +1,13 @@
 package com.alinesno.infra.data.assets.api.controller;
 
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
-import com.alinesno.infra.common.core.utils.StringUtils;
-import com.alinesno.infra.common.facade.pageable.ConditionDto;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import com.alinesno.infra.data.assets.entity.LabelEntity;
-import com.alinesno.infra.data.assets.service.IAssetCatalogService;
 import com.alinesno.infra.data.assets.service.ILabelService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 /**
  *
@@ -43,12 +40,23 @@ public class LabelController extends BaseController<LabelEntity, ILabelService> 
      * @param page DatatablesPageBean对象。
      * @return 包含DataTables数据的TableDataInfo对象。
      */
+    @DataPermissionScope
     @ResponseBody
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
         log.debug("page = {}", ToStringBuilder.reflectionToString(page));
 
         return this.toPage(model, this.getFeign(), page);
+    }
+
+    /**
+     * 保存标签的方法 insertLabel
+     * @return
+     */
+    @DataPermissionSave
+    @PostMapping("/insertLabel")
+    public AjaxResult insertLabel(@RequestBody LabelEntity entity) throws Exception {
+        return super.save(null , entity);
     }
 
     @Override
