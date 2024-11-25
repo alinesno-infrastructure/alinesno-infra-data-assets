20215f28d28ed0cf6423be0e84df504bdf6f26a5
李伟(Michael) <michaelAip@gmail.com>
2024-11-26 00:17:07 +0800

    feat: 在 DashboardController 中添加获取最新数据资产目录和用户密钥的功能。

--- alinesno-infra-data-assets-gateway/src/main/java/com/alinesno/infra/data/assets/api/controller/DashboardController.java
+++ alinesno-infra-data-assets-gateway/src/main/java/com/alinesno/infra/data/assets/api/controller/DashboardController.java
@@ -1,7 +1,12 @@
 package com.alinesno.infra.data.assets.api.controller;
 
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountBean;
+import com.alinesno.infra.common.web.adapter.login.annotation.CurrentAccount;
+import com.alinesno.infra.data.assets.key.KeyGenerator;
 import com.alinesno.infra.data.assets.service.IAssetCatalogService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +23,25 @@ public class DashboardController {
     @Autowired
     private IAssetCatalogService assetCatalogService ;
 
-    // 获取到最新的10条数据资产目录
+
+    /**
+     * 获取到最新的10条数据资产目录
+     * @param query
+     * @return
+     */
+    @DataPermissionQuery
     @RequestMapping("/topCatalog")
-    public AjaxResult topCatalog() {
-        return AjaxResult.success(assetCatalogService.topCatalog(10)) ;
+    public AjaxResult topCatalog(PermissionQuery query) {
+        return AjaxResult.success(assetCatalogService.topCatalog(10 , query)) ;
+    }
+
+    /**
+     * 获取到用户密钥
+      */
+    @RequestMapping("/getApiKey")
+    public AjaxResult getApiKey(@CurrentAccount CurrentAccountBean account) {
+        log.info("获取到用户密钥") ;
+        String apiKey = KeyGenerator.generateKey(String.valueOf(account.getId()) ,String.valueOf(account.getOrgId())) ;
+        return AjaxResult.success("操作成功" , apiKey) ;
     }
 }
