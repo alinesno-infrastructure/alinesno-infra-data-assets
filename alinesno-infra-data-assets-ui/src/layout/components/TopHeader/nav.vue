a50823e6cf4e40f20ef9af3afc82d29b0b9c47a1
林晓(Lily) <lilyAip@gmail.com>
2025-06-06 02:25:38 +0800

    feat: 增加专家入口并优化顶部导航样式与功能

--- alinesno-infra-data-assets-ui/src/layout/components/TopHeader/nav.vue
+++ alinesno-infra-data-assets-ui/src/layout/components/TopHeader/nav.vue
@@ -2,14 +2,21 @@
   <nav class=" header-text">
     <div class="acp-header-item ">
       <router-link class="header-label-text" to="/index">
-        <i class="fa-solid fa-screwdriver-wrench"></i> 控制台
+        <i class="fa-solid fa-screwdriver-wrench"></i> 控制台 
       </router-link>
     </div>
+    <!-- 
     <div class="acp-header-item ">
       <router-link class="header-label-text" to="/dashboard/learnPanel">
         <i class="fa-regular fa-file-word"></i> 手册
       </router-link>
     </div>
+    <div class="acp-header-item ">
+      <router-link class="header-label-text" to="/dashboard/smartService">
+        <i class="fa-solid fa-swatchbook"></i> 专家
+      </router-link>
+    </div> 
+    -->
     <div class="acp-header-item ">
       <router-link class="header-label-text" to="/dashboard/suportTechnique">
         <i class="fa-solid fa-user-tag"></i> 服务
@@ -27,18 +34,19 @@
           </el-icon>
         </a>
         <a class="header-label-text" target="_blank">
-          <img src="http://data.linesno.com/switch_header.png" class="su70ez-0 CB-gLgKdv" alt="" />
+          <!-- <img src="http://data.linesno.com/switch_header.png" class="su70ez-0 CB-gLgKdv" alt="" /> -->
+          <img :src="userStore.avatar" class="su70ez-0 CB-gLgKdv" alt="" />
         </a>
       </div>
 
       <template #dropdown>
         <el-dropdown-menu style="width: 350px">
 
-          <el-container style="margin-bottom: 15px">
+          <el-container style="margin-bottom: 0px">
             <el-header class="bg-color-base info-h" style="">
               <p class="color-text-secondary f-e-s">登陆名: {{ userStore.name }}
                 <span class="copy-user-id">
-                  <i class="fa-solid fa-clone"></i>
+                  <i class="fa-solid fa-clone"></i> 
                 </span>
               </p>
               <p class="color-text-primary f-e-s" v-if="userStore.org">组织: {{ userStore.org.orgName }} </p>
@@ -95,7 +103,7 @@ import useUserStore from '@/store/modules/user'
 const userStore = useUserStore()
 const router = useRouter();
 
-// const avatar = ref('http://data.linesno.com/switch_header.png') ;
+// const avatar = ref('http://data.linesno.com/switch_header.png') ; 
 // const nickname = ref('超级管理员') ;
 // const name = ref('超级管理员') ;
 
@@ -115,7 +123,7 @@ function logout() {
 
 /** 判断用户是否已经存在组织 */
 function handleInOrg(){
-  let org = userStore.org ;
+  let org = userStore.org ;   
   if(!org){ // 如果不存在则跳转到组织配置界面
     router.push('/createOrg')
   }
@@ -125,10 +133,10 @@ onMounted(() => {
   handleInOrg()
 })
 
-console.log('avatar = ' + userStore.avatar)
-console.log('name = ' + userStore.name)
-console.log('dept = ' + JSON.stringify(userStore.dept))
-console.log('role = ' + JSON.stringify(userStore.roles))
+// console.log('avatar = ' + userStore.avatar)
+// console.log('name = ' + userStore.name)
+// console.log('dept = ' + JSON.stringify(userStore.dept))
+// console.log('role = ' + JSON.stringify(userStore.roles))
 
 </script>
 
