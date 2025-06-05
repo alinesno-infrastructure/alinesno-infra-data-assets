8b187a549c9b649a20511028c80a9a2292976888
林晓(Lily) <lilyAip@gmail.com>
2025-06-06 02:25:38 +0800

    fix: 修复用户头像路径处理逻辑，改为直接使用 base64 数据

--- alinesno-infra-data-assets-ui/src/store/modules/user.js
+++ alinesno-infra-data-assets-ui/src/store/modules/user.js
@@ -68,9 +68,8 @@ const useUserStore = defineStore(
       getInfo() {
         return new Promise((resolve, reject) => {
           getInfo().then(res => {
-
             const user = res.user
-            const avatar = (user.avatar == "" || user.avatar == null) ? defAva : import.meta.env.VITE_APP_BASE_API + user.avatar;
+            const avatar = (user.avatar == "" || user.avatar == null) ? defAva : 'data:image/jpeg;base64,' + user.avatar;
 
             if (res.roles && res.roles.length > 0) { // 验证返回的roles是否是一个非空数组
               this.roles = res.roles
@@ -87,20 +86,6 @@ const useUserStore = defineStore(
             this.org = user.org
 
             resolve(res)
-
-            // const user = res.user
-            // const avatar = (user.avatar == "" || user.avatar == null) ? defAva : import.meta.env.VITE_APP_BASE_API + user.avatar;
-
-            // if (res.roles && res.roles.length > 0) { // 验证返回的roles是否是一个非空数组
-            //   this.roles = res.roles
-            //   this.permissions = res.permissions
-            // } else {
-            //   this.roles = ['ROLE_DEFAULT']
-            // }
-            // this.name = user.userName
-            // this.avatar = avatar;
-            // resolve(res)
-
           }).catch(error => {
             reject(error)
           })
