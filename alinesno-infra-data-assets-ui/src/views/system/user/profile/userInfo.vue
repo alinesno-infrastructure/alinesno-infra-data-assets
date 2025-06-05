b91e2280a99bc2f57397e556d229de480c27aeae
林晓(Lily) <lilyAip@gmail.com>
2025-06-06 02:25:38 +0800

    fix:修复 userInfo.vue 中用户昵称和手机号字段的命名不一致问题，并调整校验规则字段名

--- alinesno-infra-data-assets-ui/src/views/system/user/profile/userInfo.vue
+++ alinesno-infra-data-assets-ui/src/views/system/user/profile/userInfo.vue
@@ -1,10 +1,10 @@
 <template>
    <el-form ref="userRef" :model="user" :rules="rules" label-width="80px">
-      <el-form-item label="用户昵称" prop="nickName">
-         <el-input v-model="user.nickName" maxlength="30" />
+      <el-form-item label="用户昵称" prop="name">
+         <el-input v-model="user.name" maxlength="30" />
       </el-form-item>
-      <el-form-item label="手机号码" prop="phonenumber">
-         <el-input v-model="user.phonenumber" maxlength="11" />
+      <el-form-item label="手机号码" prop="phone">
+         <el-input v-model="user.phone" maxlength="11" />
       </el-form-item>
       <el-form-item label="邮箱" prop="email">
          <el-input v-model="user.email" maxlength="50" />
@@ -34,15 +34,16 @@ const props = defineProps({
 const { proxy } = getCurrentInstance();
 
 const rules = ref({
-  nickName: [{ required: true, message: "用户昵称不能为空", trigger: "blur" }],
+  name: [{ required: true, message: "用户昵称不能为空", trigger: "blur" }],
   email: [{ required: true, message: "邮箱地址不能为空", trigger: "blur" }, { type: "email", message: "请输入正确的邮箱地址", trigger: ["blur", "change"] }],
-  phonenumber: [{ required: true, message: "手机号码不能为空", trigger: "blur" }, { pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: "请输入正确的手机号码", trigger: "blur" }],
+  phone: [{ required: true, message: "手机号码不能为空", trigger: "blur" }, { pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: "请输入正确的手机号码", trigger: "blur" }],
 });
 
 /** 提交按钮 */
 function submit() {
   proxy.$refs.userRef.validate(valid => {
     if (valid) {
+      props.user.avatarPath = '';
       updateUserProfile(props.user).then(response => {
         proxy.$modal.msgSuccess("修改成功");
       });
