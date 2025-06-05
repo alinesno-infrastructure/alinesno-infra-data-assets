779d39fd3c8cd52372874424c028dd65906601ed
林晓(Lily) <lilyAip@gmail.com>
2025-06-06 02:25:38 +0800

    feat: 优化 TopHeader banner 组件，重构代码结构并提升可读性

--- alinesno-infra-data-assets-ui/src/layout/components/TopHeader/banner.vue
+++ alinesno-infra-data-assets-ui/src/layout/components/TopHeader/banner.vue
@@ -14,44 +14,25 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
+import saasLogo from '@/assets/logo/logo.png'  // 先导入图片
 
-export default {
-  name: 'TopHeader',
-  components: {
-  },
-  computed: {
-  },
-  data() {
+const saasTitle = ref('AIP智能设施')
+const enableLogo = ref(true)
+const saasUrl = ref('http://alinesno-infra-plat-console-admin.beta.plat.infra.linesno.com')
+const saasLogoUrl = ref(saasLogo)  // 使用导入的图片
+const displayUrl = ref('')
+const domainName = ref(null)
 
-    let saasTitle = 'AIP智能设施'
-    let enableLogo = true;
-    let saasUrl = 'http://alinesno-infra-plat-console-admin.beta.plat.infra.linesno.com' ;
-    let saasLogoUrl = 'http://portal.infra.linesno.com/logo.png' ; 
-    let displayUrl = ''; 
+const dashboardHome = () => {
+  window.location.href = saasUrl.value
+}
 
-
-    return {
-      saasTitle,
-      saasUrl,
-      saasLogoUrl,
-      enableLogo,
-      displayUrl,
-      domainName: null,
-    }
-  },
-  created() {
-  },
-  methods: {
-    dashboardHome() {
-      window.location.href = this.saasUrl
-    },
-    // 进入企业官网
-    enterDomain() {
-      if (this.domainName) { // 跳转进入官网
-        window.open(this.domainName)
-      }
-    }
+// 进入企业官网
+const enterDomain = () => {
+  if (domainName.value) { // 跳转进入官网
+    window.open(domainName.value)
   }
 }
 </script>
