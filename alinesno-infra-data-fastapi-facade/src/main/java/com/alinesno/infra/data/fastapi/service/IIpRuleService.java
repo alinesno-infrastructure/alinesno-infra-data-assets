dc9d8a06951f844c2e2abff8a5026db072499db6
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:27 +0800

    更新:alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IIpRuleService.java

--- alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IIpRuleService.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.data.fastapi.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.data.fastapi.entity.IpRuleEntity;
-
-public interface IIpRuleService extends IBaseService<IpRuleEntity> {
-}
