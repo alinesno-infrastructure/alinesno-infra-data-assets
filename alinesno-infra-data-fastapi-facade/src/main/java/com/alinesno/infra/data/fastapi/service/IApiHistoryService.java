3297f53f7ab140edb853378b9280ff7a1f38c107
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:27 +0800

    更新:alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IApiHistoryService.java

--- alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IApiHistoryService.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.data.fastapi.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.data.fastapi.entity.ApiHistoryEntity;
-
-public interface IApiHistoryService extends IBaseService<ApiHistoryEntity> {
-}
