b4e512d127a65a77c0655c5af8bb7c1e5dcdb546
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:29 +0800

    fix:移除不必要的导入语句AssetCollectionEntity

--- alinesno-infra-data-assets-domain/src/main/java/com/alinesno/infra/data/assets/mapper/AssetDataMapper.java
+++ alinesno-infra-data-assets-domain/src/main/java/com/alinesno/infra/data/assets/mapper/AssetDataMapper.java
@@ -2,7 +2,6 @@ package com.alinesno.infra.data.assets.mapper;
 
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.data.assets.entity.AssetCollectionEntity;
 import com.alinesno.infra.data.assets.entity.AssetDataEntity;
 
 /**
