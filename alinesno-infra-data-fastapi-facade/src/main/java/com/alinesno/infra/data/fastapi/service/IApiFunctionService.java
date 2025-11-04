d8822876630247e64c0172f942407b1d2663fdd1
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:27 +0800

    更新:alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IApiFunctionService.java

--- alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IApiFunctionService.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.data.fastapi.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.data.fastapi.entity.ApiFunctionEntity;
-
-public interface IApiFunctionService extends IBaseService<ApiFunctionEntity> {
-}
