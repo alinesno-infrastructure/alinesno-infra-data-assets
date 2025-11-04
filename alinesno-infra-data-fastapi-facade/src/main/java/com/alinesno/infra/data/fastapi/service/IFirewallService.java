a05f880830e8ff6c94a62b1a98c259fa3b73ebdd
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:26 +0800

    更新:alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IFirewallService.java

--- alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/service/IFirewallService.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.data.fastapi.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.data.fastapi.entity.FirewallEntity;
-
-public interface IFirewallService extends IBaseService<FirewallEntity> {
-}
