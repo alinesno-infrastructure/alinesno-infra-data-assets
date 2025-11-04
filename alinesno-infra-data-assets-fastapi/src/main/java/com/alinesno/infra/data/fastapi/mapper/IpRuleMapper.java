277d0125122628dee5311427aca66702d47b8c1e
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:28 +0800

    feat:创建IpRuleMapper接口，用于操作IP规则实体

---
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/mapper/IpRuleMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.fastapi.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.fastapi.entity.IpRuleEntity;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+public interface IpRuleMapper extends IBaseMapper<IpRuleEntity> {
+
+}
