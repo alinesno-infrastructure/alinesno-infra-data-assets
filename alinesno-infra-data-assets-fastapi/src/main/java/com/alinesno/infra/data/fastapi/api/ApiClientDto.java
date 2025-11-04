e94fcdf95fbcdcd514aa1b2879e59a638ac6d22a
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:28 +0800

    feat:创建ApiClientDto类，包含客户端基本信息和验证注解

---
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/api/ApiClientDto.java
@@ -0,0 +1,48 @@
+package com.alinesno.infra.data.fastapi.api;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * API客户端
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ApiClientDto extends BaseDto {
+
+    /**
+     * 客户端ID
+     */
+    private String clientId;
+
+    /**
+     * 客户端名称
+     */
+    @NotBlank(message = "Client Name cannot be blank.")
+    private String clientName;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+    /**
+     * 密钥
+     */
+    @NotBlank(message = "Secret cannot be blank.")
+    private String secret;
+
+    /**
+     * Token过期时间
+     */
+    @NotNull(message = "Expiry Time cannot be null.")
+    private String expiryTimeStr ;
+
+    /**
+     * 调用次数
+     */
+    private Integer useCount = 0;
+}
