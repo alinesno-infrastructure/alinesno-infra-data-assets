93d09bcb227fbf650887fa85cf718ada49e01a43
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:28 +0800

    feat:创建IIpRuleService接口，继承IBaseService<IpRuleEntity>

---
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/service/IIpRuleService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.fastapi.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.fastapi.entity.IpRuleEntity;
+
+public interface IIpRuleService extends IBaseService<IpRuleEntity> {
+}
