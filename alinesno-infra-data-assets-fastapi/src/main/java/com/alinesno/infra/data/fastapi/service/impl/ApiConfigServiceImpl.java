0cb4035a32e8b3342bb25d87ca320284ad89f1bb
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:28 +0800

    feat:创建ApiConfigServiceImpl类，实现根据uri查询接口配置功能。

---
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/service/impl/ApiConfigServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.data.fastapi.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.data.fastapi.entity.ApiConfigEntity;
+import com.alinesno.infra.data.fastapi.mapper.ApiConfigMapper;
+import com.alinesno.infra.data.fastapi.service.IApiConfigService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ApiConfigServiceImpl extends IBaseServiceImpl<ApiConfigEntity, ApiConfigMapper> implements IApiConfigService {
+
+    @Override
+    public ApiConfigEntity getByUri(String uri) {
+        return this.lambdaQuery().eq(ApiConfigEntity::getPath, uri).one() ;
+    }
+
+
+}
