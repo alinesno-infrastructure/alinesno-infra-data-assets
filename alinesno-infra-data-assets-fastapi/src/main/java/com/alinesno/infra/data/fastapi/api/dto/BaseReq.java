53ccc8681b70d1242871fe3df56b19cae3423dc5
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:28 +0800

    feat:创建BaseReq类，包含操作员ID属性

---
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/api/dto/BaseReq.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.data.fastapi.api.dto;
+
+import lombok.Data;
+
+/**
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Data
+public class BaseReq {
+
+    //  操作员
+    String operatorId;
+
+}
