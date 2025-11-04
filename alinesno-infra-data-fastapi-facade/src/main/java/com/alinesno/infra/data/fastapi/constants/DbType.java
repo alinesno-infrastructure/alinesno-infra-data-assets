eae05ffe6f585de0558455f69c2a7fdfaecab912
李娜(Allen) <allenAip@gmail.com>
2025-11-05 00:29:26 +0800

    更新:alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/constants/DbType.java

--- alinesno-infra-data-fastapi-facade/src/main/java/com/alinesno/infra/data/fastapi/constants/DbType.java
+++
@@ -1,6 +0,0 @@
-package com.alinesno.infra.data.fastapi.constants;
-
-public class DbType {
-    public static final String MY_SQL ="MySQL";
-    public static final String MYSQL_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-}
