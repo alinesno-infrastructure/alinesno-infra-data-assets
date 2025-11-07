500c3cdb2db8571312958169ce604cb136d49f41
王磊(Levi) <leviAip@gmail.com>
2025-11-08 00:19:15 +0800

    feat:创建数据源配置Mapper接口

---
+++ alinesno-infra-data-assets-domain/src/main/java/com/alinesno/infra/data/assets/mapper/DataSourceConfigMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.data.assets.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.assets.entity.DataSourceConfigEntity;
+
+public interface DataSourceConfigMapper extends IBaseMapper<DataSourceConfigEntity> {
+
+}
