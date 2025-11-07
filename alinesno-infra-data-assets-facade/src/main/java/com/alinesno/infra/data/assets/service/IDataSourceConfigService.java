a80f598196d15197a9dbd63495e166a740132a2b
王磊(Levi) <leviAip@gmail.com>
2025-11-08 00:19:15 +0800

    feat:创建数据源配置服务接口IDataSourceConfigService

---
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/service/IDataSourceConfigService.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.assets.service;
+
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.assets.entity.DataSourceConfigEntity;
+
+public interface IDataSourceConfigService extends IBaseService<DataSourceConfigEntity> {
+
+    /**
+     * 获取当前数据源配置
+     * @param permissionQuery
+     * @return
+     */
+    DataSourceConfigEntity getCurrentDatasourceConfig(PermissionQuery permissionQuery);
+
+}
