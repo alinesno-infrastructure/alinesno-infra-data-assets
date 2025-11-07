ef5643daa8f5504861c01a81d33b74c78517cee0
王磊(Levi) <leviAip@gmail.com>
2025-11-08 00:19:15 +0800

    feat:重构产品侧边栏菜单，支持动态数据绑定和图标渲染

--- alinesno-infra-data-assets-ui/src/layout/components/ProductSidebar/index.vue
+++ alinesno-infra-data-assets-ui/src/layout/components/ProductSidebar/index.vue
@@ -1,67 +1,72 @@
 <template>
   <div class="siderbar">
-    <el-menu default-active="1" class="el-menu-vertical" :collapse="isCollapse" @open="handleOpen" @close="handleClose">
-      <el-menu-item index="1" @click="jumpTo">
-        <i class="fa-solid fa-desktop"></i>
-      </el-menu-item>
+    <el-menu :default-active="activeMenu" class="el-menu-vertical" :collapse="isCollapse" @open="handleOpen" @close="handleClose">
 
-      <el-menu-item index="2" @click="openServiceList">
-        <i class="fa-solid fa-sailboat"></i>
-      </el-menu-item>
-
-      <el-menu-item index="3" @click="openSmartService">
-        <i class="fa-solid fa-user-shield"></i>
-      </el-menu-item>
+        <el-menu-item :index="item.id" @click="openServiceList(item.link)" v-for="item in menuItems" :key="item.id"  class="aip-menu-item">
+          <i :class="item.icon"></i>
+          <span>
+            {{ item.desc }}
+          </span>
+        </el-menu-item>
     </el-menu>
 
     <el-menu style="" class="el-menu-vertical acp-suggest" :collapse="isCollapse" @open="handleOpen" @close="handleClose">
-      <el-menu-item index="11" @click="jumpToConstomTheme">
-        <i class="fa-solid fa-file-pen"></i>
-      </el-menu-item>
-      <el-menu-item index="12" @click="dialogVisible = true">
-        <i class="fa-solid fa-paper-plane"></i>
+        <el-menu-item :index="item.id" @click="openServiceList(item.link)" class="aip-menu-item" v-for="item in footerMenuItems" :key="item.id">
+          <i :class="item.icon"></i>
+          <span>
+            {{ item.desc }}
+          </span>
       </el-menu-item>
     </el-menu>
-
-    <!-- 建议和反馈 -->
-    <el-dialog v-model="dialogVisible" title="使用建议和反馈" width="30%" :append-to-body="true" :before-close="handleClose">
-
-      <el-form ref="ruleFormRef" label-position="top" :model="ruleForm" :rules="rules" label-width="120px"
-        class="demo-ruleForm" :size="formSize" status-icon>
-
-        <el-form-item label="您对控制台首页满意吗？" prop="name">
-          <el-rate v-model="value2" :colors="colors" />
-        </el-form-item>
-
-        <el-form-item label="您对控制台首页满意吗？" prop="name">
-          <el-input type="textarea" />
-        </el-form-item>
-
-        <el-form-item style="margin-top:30px">
-          <el-button type="primary" @click="submitForm(ruleFormRef)">保存</el-button>
-          <el-button @click="resetForm(ruleFormRef)">重置</el-button>
-        </el-form-item>
-
-      </el-form>
-
-    </el-dialog>
 
   </div>
 </template>
 
 <script setup>
 
+const createChildComp = ref(null);
+const createScreenComp = ref(null);
+
 const dialogVisible = ref(false)
+
 const router = useRouter();
+const route = useRoute();
+
+// 菜单列表
+const menuItems = ref([
+  { id: '1', icon: 'fa-solid fa-house-user', link: '/index', desc: '仪表盘' }, // 仪表盘用gauge更准确
+  { id: '2', icon: 'fa-solid fa-list-check', link: '/asset/data/asset/manifest/index', desc: '资产清单' }, // 清单用list-check
+  { id: '3', icon: 'fa-solid fa-sitemap', link: '/asset/data/asset/type/index', desc: '域管理' }, // 域管理用sitemap
+  { id: '4', icon: 'fa-solid fa-chart-line', link: '/metrics/data/asset/metric/index', desc: '指标管理' }, // 指标用chart-line
+  { id: '5', icon: 'fa-solid fa-server', link: '/service/data/fastapi/group/index', desc: '数据服务' }, // 服务用server
+  { id: '12', icon: 'fa-solid fa-route', link: '/lift/data/lift/data/index', desc: '生命周期' }, // 生命周期用route表示流程
+]);
+
+// Footer菜单列表
+const footerMenuItems = ref([
+  { id: '15', icon: 'fa-solid fa-code', link: '/service/data/fastapi/apiclient/index', desc: 'API客户端' }, // API用code
+  { id: '16', icon: 'fa-solid fa-calculator', link: '/operate/data/operate/analyse/index', desc: '计量统计' }, // 计量用calculator
+  { id: '13', icon: 'fa-solid fa-lock', link: '/asset/data/asset/security/index', desc: '安全策略' }, // 安全用lock
+]);
+
+// 打开服务市场
+function openServiceList(_path) {
+  router.push({ path: _path });
+}
 
 // 打开客户配置
 function jumpToConstomTheme() {
   router.push({ path: "/dashboard/dashboardTheme" });
 }
 
-// 打开服务市场
-function openServiceList() {
-  router.push({ path: "/dashboard/serviceList" });
+// 添加频道
+function addChannel(){
+  createChildComp.value.handleOpenChannel(true);
+}
+
+// 添加场景
+function addScreen(){
+  createScreenComp.value.handleOpenChannel(true);
 }
 
 // 打开首页
@@ -74,11 +79,19 @@ function openSmartService() {
   router.push({ path: "/dashboard/smartService" });
 }
 
+const activeMenu = computed(() => {
+  const currentPath = route.path
+  const item = menuItems.value.find(item => {
+    return currentPath.startsWith(item.link) // 匹配路由前缀
+  })
+  return item ? item.id : '1'
+})
+
 </script>
 
 <style lang="scss" scoped>
 .el-menu-vertical:not(.el-menu--collapse) {
-  width: 65px;
+  width: 70px;
 }
 
 .acp-suggest {
@@ -89,11 +102,31 @@ function openSmartService() {
 .siderbar {
   float: left;
   height: 100%;
-  width: 64px;
+  width: 70px;
   border-right: 1px solid #e6e6e6;
   padding-top: 40px;
   overflow: hidden;
   background-color: #fff;
   position: fixed;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  z-index: 10;
+}
+
+.aip-menu-item {
+
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  line-height: 1.2rem;
+  padding-top: 0px;
+  margin: 5px;
+  border-radius: 10px;
+  justify-content: center;
+
+  span{
+    font-size:12px;
+    color: #888;
+  }
 }
 </style>
