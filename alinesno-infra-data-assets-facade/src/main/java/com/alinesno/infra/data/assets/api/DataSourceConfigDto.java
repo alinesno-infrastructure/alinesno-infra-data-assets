4f53684800130775804b7a829b6be619ba9d72be
张婷(Tina) <tinaAip@gmail.com>
2025-11-09 00:20:24 +0800

    feat:修改DataSourceConfigDto，新增toEntity方法参数驱动类名

--- alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/api/DataSourceConfigDto.java
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/api/DataSourceConfigDto.java
@@ -3,7 +3,6 @@ package com.alinesno.infra.data.assets.api;
 import com.alinesno.infra.common.facade.base.BaseDto;
 import com.alinesno.infra.data.assets.entity.DataSourceConfigEntity;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -32,32 +31,25 @@ public class DataSourceConfigDto extends BaseDto {
     @NotBlank(message = "密码不能为空")
     private String password;
 
-    private String driverClassName;
-
     private Integer connectionTimeout;
 
-    private Integer maxPoolSize;
-
     private String remark;
-
-    @NotNull(message = "状态不能为空")
-    private Integer status;
 
     /**
      * 转换为实体对象
      * @return
      */
-    public DataSourceConfigEntity toEntity() {
+    public DataSourceConfigEntity toEntity(String driver) {
         DataSourceConfigEntity entity = new DataSourceConfigEntity();
 
         BeanUtils.copyProperties(this, entity);
 
+        entity.setDriverClassName(driver);
         entity.setName(this.name);
         entity.setType(this.type);
         entity.setUrl(this.url);
         entity.setUsername(this.username);
         entity.setPassword(this.password);
-        entity.setDriverClassName(this.driverClassName);
         entity.setConnectionTimeout(this.connectionTimeout);
         entity.setRemark(this.remark);
 
