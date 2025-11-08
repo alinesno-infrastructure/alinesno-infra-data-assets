f26be501e3761e1e5abda47c5070bad3f52e0442
张婷(Tina) <tinaAip@gmail.com>
2025-11-09 00:20:24 +0800

    feat:优化数据源配置获取方法，支持根据机构ID获取当前配置

--- alinesno-infra-data-assets-domain/src/main/java/com/alinesno/infra/data/assets/service/impl/DataSourceConfigServiceImpl.java
+++ alinesno-infra-data-assets-domain/src/main/java/com/alinesno/infra/data/assets/service/impl/DataSourceConfigServiceImpl.java
@@ -16,10 +16,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class DataSourceConfigServiceImpl extends IBaseServiceImpl<DataSourceConfigEntity, DataSourceConfigMapper> implements IDataSourceConfigService {
 
+    /**
+     * 获取当前数据源配置
+     * @param permissionQuery
+     * @return
+     */
     @Override
     public DataSourceConfigEntity getCurrentDatasourceConfig(PermissionQuery permissionQuery) {
+        return this.getCurrentDatasourceConfig(permissionQuery.getOrgId());
+    }
+
+    /**
+     * 获取当前数据源配置
+     * @param orgId
+     * @return
+     */
+    @Override
+    public DataSourceConfigEntity getCurrentDatasourceConfig(Long orgId) {
         LambdaQueryWrapper<DataSourceConfigEntity> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(DataSourceConfigEntity::getOrgId, permissionQuery.getOrgId());
+        queryWrapper.eq(DataSourceConfigEntity::getOrgId, orgId);
         return this.getOne(queryWrapper);
     }
 
