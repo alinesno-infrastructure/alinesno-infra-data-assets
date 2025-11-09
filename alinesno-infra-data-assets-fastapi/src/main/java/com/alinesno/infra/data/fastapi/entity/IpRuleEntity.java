209d7fb25bae7993e309037ee1815c9587c01b8a
王磊(Levi) <leviAip@gmail.com>
2025-11-10 00:20:18 +0800

    feat:添加IpRuleEntity类，包含表注解和基础属性

--- alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/entity/IpRuleEntity.java
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/entity/IpRuleEntity.java
@@ -4,13 +4,18 @@ import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
 import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * IpRuleEntity类
+ */
 @EqualsAndHashCode(callSuper = true)
 @TableName("ip_rule")
 @Data
+@TableComment("API接口安全规则表")
 public class IpRuleEntity extends InfraBaseEntity {
 
     /**
