fee00ca0ea83c111e6d7161abe569a70befaf839
王磊(Levi) <leviAip@gmail.com>
2025-11-10 00:20:18 +0800

    feat:优化ManifestFieldEntity类字段定义及注释

--- alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/entity/ManifestFieldEntity.java
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/entity/ManifestFieldEntity.java
@@ -13,35 +13,62 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @TableName("assets_manifest_field")
-@TableComment("数据清单")
+@TableComment("数据资产字段表")
 public class ManifestFieldEntity extends InfraBaseEntity {
 
-    @Column(length = 32 , comment = "清单ID" , isNull = false)
     @TableField("manifest_id")
-    private Long manifestId = 0L;
+    @Column(comment = "资产登记表ID")
+    private Long manifestId;
 
-    @Column(length = 50 , comment = "字段名" , isNull = false)
-    @TableField
+    @TableField("field_name")
+    @Column(length = 200 , comment = "字段名")
     private String fieldName;
 
-    @Column(length = 255 , comment = "字段类型" , isNull = false)
-    @TableField
+    @TableField("field_type")
+    @Column(length = 100 , comment = "字段类型")
     private String fieldType;
 
-    @Column(length = 11 , comment = "字段长度" , isNull = false)
-    @TableField
+    @TableField("filed_length")
+    @Column(comment = "字段长度")
     private Integer filedLength;
 
-    @Column(length = 50 , comment = "是否为空")
-    @TableField
+    @TableField("numeric_precision")
+    @Column(comment = "数值精度")
+    private Integer numericPrecision;
+
+    @TableField("numeric_scale")
+    @Column(comment = "小数位")
+    private Integer numericScale;
+
+    @TableField("is_nullable")
+    @Column(comment = "是否可为空")
     private Boolean isNullable;
 
-    @Column(length = 50 , comment = "是否为主键")
-    @TableField
+    @TableField("is_primary_key")
+    @Column(comment = "是否主键")
     private Boolean isPrimaryKey;
 
-    // 字段注释，可选
-    @TableField
-    @Column(length = 512 , comment = "字段注释")
+    @TableField("is_auto_increment")
+    @Column(comment = "是否自增")
+    private Boolean isAutoIncrement;
+
+    @TableField("default_value")
+    @Column(length = 500 , comment = "默认值")
+    private String defaultValue;
+
+    @TableField("field_comment")
+    @Column(length = 500 , comment = "字段备注")
     private String fieldComment;
+
+    @TableField("character_set_name")
+    @Column(length = 200 , comment = "字符集")
+    private String characterSetName;
+
+    @TableField("collation_name")
+    @Column(length = 200 , comment = "排序规则")
+    private String collationName;
+
+    @TableField("org_id")
+    @Column(comment = "组织ID")
+    private Long orgId;
 }
