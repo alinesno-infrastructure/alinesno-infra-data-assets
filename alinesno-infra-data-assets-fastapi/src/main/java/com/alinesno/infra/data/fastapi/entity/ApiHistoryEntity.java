84641dee3baf4c9c7e526a025a095872ab51d0b5
王磊(Levi) <leviAip@gmail.com>
2025-11-10 00:20:18 +0800

    feat:添加ApiHistoryEntity类，包含表注解和基础属性

--- alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/entity/ApiHistoryEntity.java
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/entity/ApiHistoryEntity.java
@@ -5,12 +5,17 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * ApiHistoryEntity类
+ */
 @EqualsAndHashCode(callSuper = true)
 @TableName("api_history")
 @Data
+@TableComment("API接口历史表")
 public class ApiHistoryEntity extends InfraBaseEntity {
 
     /**
