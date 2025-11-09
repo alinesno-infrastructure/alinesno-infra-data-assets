c782a7e6dc9c14a17a235c99ee39c589d85a0df5
王磊(Levi) <leviAip@gmail.com>
2025-11-10 00:20:18 +0800

    feat:添加数据资产统计表注解@TableComment

--- alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/entity/AssetStatisticsEntity.java
+++ alinesno-infra-data-assets-facade/src/main/java/com/alinesno/infra/data/assets/entity/AssetStatisticsEntity.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -21,6 +22,7 @@ import java.math.BigDecimal;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @TableName("asset_statistics")
+@TableComment("数据资产统计表")
 public class AssetStatisticsEntity extends InfraBaseEntity {
     // fields
     //getter and setter
