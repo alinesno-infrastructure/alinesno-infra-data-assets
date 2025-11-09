b60e8cf8fbd8ee579d731c780b230a8ec2ebdf52
王磊(Levi) <leviAip@gmail.com>
2025-11-10 00:20:18 +0800

    docs:添加@ApiFunctionEntity类的表注释说明

--- alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/entity/ApiFunctionEntity.java
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/entity/ApiFunctionEntity.java
@@ -2,11 +2,13 @@ package com.alinesno.infra.data.fastapi.entity;
 
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @TableName("api_function")
 @Data
+@TableComment("API接口功能列表")
 public class ApiFunctionEntity extends InfraBaseEntity {
 }
