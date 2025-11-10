566b5d7cc245cf53efc0f95fabc1bdfe1f90c6c7
周健(James) <jamesAip@gmail.com>
2025-11-11 00:24:02 +0800

    feat:新增根据路径和组织ID查询接口配置方法

--- alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/service/impl/ApiConfigServiceImpl.java
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/service/impl/ApiConfigServiceImpl.java
@@ -4,8 +4,11 @@ import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.data.fastapi.entity.ApiConfigEntity;
 import com.alinesno.infra.data.fastapi.mapper.ApiConfigMapper;
 import com.alinesno.infra.data.fastapi.service.IApiConfigService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.lang.exception.RpcServiceRuntimeException;
 
 @Slf4j
 @Service
@@ -16,5 +19,23 @@ public class ApiConfigServiceImpl extends IBaseServiceImpl<ApiConfigEntity, ApiC
         return this.lambdaQuery().eq(ApiConfigEntity::getPath, uri).one() ;
     }
 
+    @Override
+    public ApiConfigEntity getByUri(String path, Long orgId) {
+        if (path == null){
+            return null;
+        }
+
+        // 1. 优先按 orgId 查找（这里示例把 ApiConfigEntity.groupId 等同为 orgId，如你表中是其他字段请替换）
+        if (orgId == null) {
+            throw new RpcServiceRuntimeException("路径["+path+"]组织编号为空") ;
+        }
+
+        LambdaQueryWrapper<ApiConfigEntity> qw = new LambdaQueryWrapper<>();
+        qw.eq(ApiConfigEntity::getPath, path)
+                .eq(ApiConfigEntity::getOrgId, orgId)
+                .eq(ApiConfigEntity::isEnabled, true);
+
+        return this.getOne(qw, false);
+    }
 
 }
