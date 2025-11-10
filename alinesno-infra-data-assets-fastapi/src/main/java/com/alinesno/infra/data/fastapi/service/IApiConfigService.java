b7fef9695bcaeedfb6bd0358d11b9ffed6a59aaa
周健(James) <jamesAip@gmail.com>
2025-11-11 00:24:02 +0800

    feat: 在 IApiConfigService 接口中新增根据 uri 和 orgId 获取配置的方法

--- alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/service/IApiConfigService.java
+++ alinesno-infra-data-assets-fastapi/src/main/java/com/alinesno/infra/data/fastapi/service/IApiConfigService.java
@@ -12,4 +12,9 @@ public interface IApiConfigService extends IBaseService<ApiConfigEntity> {
      */
     ApiConfigEntity getByUri(String uri);
 
+    /**
+     * 根据 uri 和 orgId 获取配置，优先匹配 org 级别的配置；若未命中可回退为全局配置（orgId 为 null 表示全局）
+     */
+    ApiConfigEntity getByUri(String uri, Long orgId);
+
 }
